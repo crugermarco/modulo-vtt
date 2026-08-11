67c449503ae58eeadd1700da09445da18aa91cfa
crugermarco <marcocrugeruchija@gmail.com>
2026-08-11 13:40:20 -0700

    se corrigieron errores que non dejaba hacer despliegue BOM

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-﻿import React, { useState, useCallback } from 'react'
+import React, { useState, useCallback } from 'react'
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
 import { Toaster } from 'react-hot-toast'
 import MainLayout from './components/Layout/MainLayout'
